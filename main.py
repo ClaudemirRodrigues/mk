ab8178b84d39196ffe2569bb0b96bbf80bd4669e
Claudemir Rodríguez <49052180+ClaudemirRodrigues@users.noreply.github.com>
2020-06-02 21:44:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ from bs4 import BeautifulSoup
 
 product_description = dict()
 products = list()
+urls = list()
+soup = list()
 
-page_makeup_lips = requests.get('https://www.marykay.com.br/pt-br/products/makeup/lips')
-soup = BeautifulSoup(page_makeup_lips.text, 'html.parser')
+for c in range(1, 4):
+    url = 'https://www.marykay.com.br/pt-br/products/makeup/lips?page=' + str(c)
+    urls.append(url)
+for item in urls:
+    page = requests.get(item)
+    soup.append(BeautifulSoup(page.text, 'html.parser'))
 
-product_all = soup.find(class_='container main')
-product_name = product_all.find_all_next(class_='product-name')
-product_details = product_all.find_all_next(class_='name')
-product_price = product_all.find_all_next(class_='price')
+for x in range(len(soup)):
+    product_all = soup[x].find(class_='container main')
+    product_name = product_all.find_all_next(class_='product-name')
+    product_details = product_all.find_all_next(class_='name')
+    product_price = product_all.find_all_next(class_='price')
 
-for c in range(0, len(product_name)):
-    description = product_name[c].contents[0]
-    tones = product_details[c].contents[0]
-    product_description['Produto'] = description
-    product_description['Tom'] = tones
-    products.append(product_description.copy())
+    for c in range(0, len(product_name)):
+        description = product_name[c].contents[0]
+        product_description['Produto'] = description
+        try:
+            tones = product_details[c].contents[0]
+            product_description['Tom'] = tones
+        except IndexError:
+            product_description['Tom'] = ''
+        products.append(product_description.copy())
 
 for product in product_price:
     price = product.contents[2]

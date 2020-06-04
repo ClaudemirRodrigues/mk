f8a88c83a71c43a343c160e7ea2cb11812ac665a
Claudemir Rodríguez <49052180+ClaudemirRodrigues@users.noreply.github.com>
2020-06-04 19:21:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,34 @@ product_description = dict()
 products = list()
 urls = list()
 soup = list()
-
+# série de loops para armazenar as urls na lista
 for c in range(1, 4):
-    url = 'https://www.marykay.com.br/pt-br/products/makeup/lips?page=' + str(c)
+    url = 'https://www.marykay.com.br/pt-br/products/new-products?page=' + str(c)
     urls.append(url)
+for c in range(1, 5):
+    url = 'https://www.marykay.com.br/pt-br/products/skincare?page=' + str(c)
+    urls.append(url)
+for c in range(1, 14):
+    url = 'https://www.marykay.com.br/pt-br/products/makeup?page=' + str(c)
+    urls.append(url)
+for c in range(1, 3):
+    url = 'https://www.marykay.com.br/pt-br/products/fragrance?page=' + str(c)
+    urls.append(url)
+url = 'https://www.marykay.com.br/pt-br/products/body-and-sun'
+urls.append(url)
+
+# laço para fazer o request das páginas
 for item in urls:
     page = requests.get(item)
     soup.append(BeautifulSoup(page.text, 'html.parser'))
 
+# laço principal para extrair os dados específicos
 for x in range(len(soup)):
     product_all = soup[x].find(class_='container main')
     product_name = product_all.find_all_next(class_='product-name')
     product_details = product_all.find_all_next(class_='name')
     product_price = product_all.find_all_next(class_='price')
+    product_sup = product_all.find_all_next('sup')
 
     for c in range(0, len(product_name)):
         description = product_name[c].contents[0]
@@ -27,10 +42,12 @@ for x in range(len(soup)):
             product_description['Tom'] = tones
         except IndexError:
             product_description['Tom'] = ''
-        products.append(product_description.copy())
+        price = product_price[c].contents[2]
 
-for product in product_price:
-    price = product.contents[2]
+        sup = product_sup[c].contents[0]
+        product_description['Preço'] = str(price) + ',' + str(sup)
+
+        products.append(product_description.copy())
 
 for p in products:
     for k, v in p.items():
